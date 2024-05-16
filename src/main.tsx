4f5e8e026f32bbe235809993c06e7a36024847ff
skoanton <antonhakanssonn@gmail.com>
2024-05-16 20:20:54 +0200

    la till provider i main

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./global.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./pages/Root/Root.tsx";
+import EventsProvider from "./Context/EventsContext/EventsProvider.tsx";
 
 const router = createBrowserRouter([
   {
@@ -35,6 +36,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <EventsProvider>
+      <RouterProvider router={router} />
+    </EventsProvider>
   </React.StrictMode>
 );
