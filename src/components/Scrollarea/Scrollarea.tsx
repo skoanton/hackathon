51f0971911e5dba5999504ff426ad329b158706d
Sebastian <selfishworm117@gmail.com>
2024-05-16 19:43:36 +0200

    karusell styling 🤢

--- src/components/Scrollarea/Scrollarea.tsx
+++ src/components/Scrollarea/Scrollarea.tsx
@@ -1,64 +1,75 @@
 import { ScrollAreaShad } from "../ui/scroll-area";
 
 //fake data temp
-const events = [
+export const events = [
   {
     title: "Maraton",
     description: "En löpartävling genom stadens gator.",
     arranger: "Borås stad",
+    thumbnail:
+      "https://i.guim.co.uk/img/media/24c74bce590f4643d5c7315efc736832592c148d/0_4_828_497/master/828.jpg?width=1200&height=1200&quality=85&auto=format&fit=crop&s=9b5cacec03ce1f238735e8300194bb88",
   },
   {
     title: "Konstutställning",
     description: "En visning av samtida konstverk från lokala konstnärer.",
     arranger: "Stadens konstgalleri",
+    thumbnail: "",
   },
   {
     title: "Musikfestival",
     description: "Tre dagar med livemusik från lokala och internationella artister.",
     arranger: "Musikevenemang AB",
+    thumbnail: "",
   },
   {
     title: "Matmässa",
     description: "Upplev ett urval av kök från hela världen.",
     arranger: "Matälskare föreningen",
+    thumbnail: "",
   },
   {
     title: "Teknikkonferens",
     description: "Utforska de senaste innovationerna och trenderna inom teknologi.",
     arranger: "Teknik Innovations AB",
+    thumbnail: "",
   },
   {
     title: "Modevisning",
     description: "En glamorös uppvisning av de senaste modetrenderna.",
     arranger: "Mode Framåt AB",
+    thumbnail: "",
   },
   {
     title: "Filmfestival",
     description: "Visningar av oberoende och internationella filmer.",
     arranger: "Bioföreningen",
+    thumbnail: "",
   },
   {
     title: "Välgörenhetsgala",
     description:
       "Samla in pengar till lokala välgörenhetsändamål samtidigt som du njuter av en kväll med underhållning.",
     arranger: "Samhällets välgörenhetsstiftelse",
+    thumbnail: "",
   },
   {
     title: "Litterärt symposium",
     description: "Diskussioner och läsningar av kända författare.",
     arranger: "Bokmalarnas förening",
+    thumbnail: "",
   },
   {
     title: "Fitnessmässa",
     description: "Upptäck det senaste inom fitness trender och utrustning.",
     arranger: "FitLife Events",
+    thumbnail: "",
   },
   {
     title: "Stand-up kväll",
     description: "Skratt garanterat med en kväll av komedi.",
     arranger: "Kvarterskomikerna",
+    thumbnail: "",
   },
-  // Add more events here as needed...
 ];
 
 export function ScrollArea() {
@@ -68,7 +79,7 @@ export function ScrollArea() {
         {events.map((event, i) => (
           <div key={i} className="text-sm border-2 border-black h-24 bg-red-200 flex gap-2 mb-2">
             <img
-              src={event.title}
+              src={event.thumbnail}
               alt={`${event.title}-thumbnail`}
               className="w-28 h-full max-w-28 min-w-28 bg-blue-200"
             />
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,4 +1,5 @@
 import BottomNavbar from "@/components/BottomNavbar/BottomNavbar";
+import { StartpageCarousel } from "@/components/Carousel/Carousel";
 import { Header } from "@/components/Header/Header";
 import { ScrollArea } from "@/components/Scrollarea/Scrollarea";
 import { Outlet } from "react-router-dom";
@@ -7,6 +8,7 @@ const Root = () => {
   return (
     <>
       <Header />
+      <StartpageCarousel />
       <ScrollArea />
       <Outlet />
       <BottomNavbar />
