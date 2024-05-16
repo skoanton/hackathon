98d42f74c9b4f8da06efa1da705739a760e3f2d0
skoanton <antonhakanssonn@gmail.com>
2024-05-16 21:20:48 +0200

    tjo

--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,19 +1,19 @@
-import BottomNavbar from '@/components/BottomNavbar/BottomNavbar';
-import { Header } from '@/components/Header/Header';
-import { Outlet } from 'react-router-dom';
+import BottomNavbar from "@/components/BottomNavbar/BottomNavbar";
+import { Header } from "@/components/Header/Header";
+import { Outlet } from "react-router-dom";
 
 const Root = () => {
-   return (
-      <>
-         <div className=" h-screen">
-            {/* <Header /> */}
-            <main>
-               <Outlet />
-            </main>
-            <BottomNavbar />
-         </div>
-      </>
-   );
+  return (
+    <>
+      <div className=" h-screen">
+        {/* <Header /> */}
+        <main>
+          <Outlet />
+        </main>
+        <BottomNavbar />
+      </div>
+    </>
+  );
 };
 
 export default Root;
