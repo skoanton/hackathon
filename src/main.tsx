cb0f270234980a21bd0767e9131f8434fdaed5fc
DennisFlink <dennis.sabriev@outlook.com>
2024-05-16 19:42:52 +0200

    kuken

--- src/main.tsx
+++ src/main.tsx
@@ -1,40 +1,40 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./global.css";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
-import Root from "./pages/Root/Root.tsx";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './global.css';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Root from './pages/Root/Root.tsx';
+import { HomePage } from './pages/Root/HomePage.tsx';
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Root />,
-    children: [
-      {
-        path: "/home",
-        element: <Root />,
-      },
-      {
-        path: "/event/:eventId",
-        element: <Root />,
-      },
-      {
-        path: "/event/new",
-        element: <Root />,
-      },
-      {
-        path: "/event/saved",
-        element: <Root />,
-      },
-      {
-        path: "/profile/:profileId",
-        element: <Root />,
-      },
-    ],
-  },
+   {
+      element: <Root />,
+      children: [
+         {
+            path: '/',
+            element: <HomePage />,
+         },
+         {
+            path: '/event/:eventId',
+            element: <Root />,
+         },
+         {
+            path: '/event/new',
+            element: <Root />,
+         },
+         {
+            path: '/event/saved',
+            element: <Root />,
+         },
+         {
+            path: '/profile/:profileId',
+            element: <Root />,
+         },
+      ],
+   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById('root')!).render(
+   <React.StrictMode>
+      <RouterProvider router={router} />
+   </React.StrictMode>
 );
--- src/pages/Root/Root.tsx
+++ src/pages/Root/Root.tsx
@@ -1,12 +1,13 @@
 import BottomNavbar from '@/components/BottomNavbar/BottomNavbar';
-
+import { Header } from '@/components/Header/Header';
 import { Outlet } from 'react-router-dom';
 
 const Root = () => {
    return (
       <>
-         <div className="h-screen border-collapse overflow-hidden">
-            <main className=" mb-24 ">
+         <div className="h-screen overflow-x-hidden">
+            <Header />
+            <main className=" container">
                <Outlet />
             </main>
             <BottomNavbar />
