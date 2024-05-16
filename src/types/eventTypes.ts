9df0f298dd3caca8e7f9c749c2d3b37976a267c2
skoanton <antonhakanssonn@gmail.com>
2024-05-16 23:36:48 +0200

    la till id och kommenterarer

--- src/types/eventTypes.ts
+++ src/types/eventTypes.ts
@@ -1,8 +1,15 @@
 export type Event = {
+    id: string,
     title: string,
     description: string,
     date: Date,
     location: string,
     organizer: string,
     images: string[],
+    comments?: EventComments[]
+}
+
+export type EventComments = {
+    name: string,
+    post: string,
 }
