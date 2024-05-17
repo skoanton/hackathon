2fafcbe720de98852c487c5a78286e585c74d55c
skoanton <antonhakanssonn@gmail.com>
2024-05-17 03:08:26 +0200

    nu kan man lägga till comments

--- src/components/EventView/EventComment.tsx
+++ src/components/EventView/EventComment.tsx
@@ -1,14 +1,36 @@
+import { EventsContext } from "@/Context/EventsContext/EventsContext";
+import { EVENT_ACTION } from "@/Context/EventsContext/EventsReducer";
 import { Button } from "@/components/ui/button";
 import { Textarea } from "@/components/ui/textarea";
+import { Event } from "@/types/eventTypes";
+import { useContext, useState } from "react";
 
-export function EventComment() {
+type EventCommentProps = {
+  currentEvent: Event;
+};
+
+export function EventComment({ currentEvent }: EventCommentProps) {
+  const { eventsDispatch } = useContext(EventsContext);
+
+  const [comment, setComment] = useState("");
+
   const submitComment = () => {
-    console.log("Commentin");
+    eventsDispatch({
+      type: EVENT_ACTION.ADD_COMMENT,
+      payload: {
+        event: currentEvent,
+        comment: comment,
+      },
+    });
   };
 
   return (
     <div className="grid w-full gap-2">
-      <Textarea placeholder="Comment on this event!!!" className="mt-2" />
+      <Textarea
+        onChange={(e) => setComment(e.target.value.toString())}
+        placeholder="Comment on this event!!!"
+        className="mt-2"
+      />
       <Button onClick={() => submitComment()}>Post comment</Button>
     </div>
   );
