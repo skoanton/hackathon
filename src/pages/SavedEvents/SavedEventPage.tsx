86cdce248851695da2829e6b5d1717b2fd92c432
skoanton <antonhakanssonn@gmail.com>
2024-05-17 04:40:18 +0200

    l

--- src/pages/SavedEvents/SavedEventPage.tsx
+++ src/pages/SavedEvents/SavedEventPage.tsx
@@ -9,10 +9,34 @@ const SavedEventPage = ({}: SavedEventPageProps) => {
 
   return (
     <>
-      <h2 className="text-xl font-bold">Upcoming event for you</h2>
-      <ScrollArea currentEventState={userEventsState.favorites} />
-      <h2 className="text-xl font-bold">Upcoming event for you</h2>
-      <ScrollArea currentEventState={userEventsState.attending} />
+      <h2 className="text-xl font-bold text-center">Upcoming event for you</h2>
+      <div
+        className={`p-2 ${
+          userEventsState.favorites.length < 1 ? "bg-primary" : "bg-transparent"
+        }`}
+      >
+        {userEventsState.favorites.length > 0 ? (
+          <ScrollArea currentEventState={userEventsState.favorites} />
+        ) : (
+          <p className="text-center font-semibold italic">
+            Du har inga sparade event ännu
+          </p>
+        )}
+      </div>
+      <h2 className="text-xl font-bold text-center">Upcoming event for you</h2>
+      <div
+        className={`p-2 ${
+          userEventsState.attending.length < 1 ? "bg-primary" : "bg-transparent"
+        }`}
+      >
+        {userEventsState.attending.length > 0 ? (
+          <ScrollArea currentEventState={userEventsState.attending} />
+        ) : (
+          <p className="text-center font-semibold italic">
+            Du har inga kommande event{" "}
+          </p>
+        )}
+      </div>
     </>
   );
 };
